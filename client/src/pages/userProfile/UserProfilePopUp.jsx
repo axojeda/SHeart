07cb8cd54336344ba4af4aae7f96b251d43e5c0b
Aura Xenia Ojeda Gomez <ax.pink@gmail.com>
2023-03-29 15:50:42 -0400

    updated

--- client/src/pages/userProfile/UserProfilePopUp.jsx
+++ client/src/pages/userProfile/UserProfilePopUp.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './userProfilePopUp.scss'
 import avatar from '../../assets/avatar.jpg'
+import chat from '../../assets/chat.png'
 
 const UserProfilePopUp = ({chatPopUp, setChatPopUp}) => {
 
@@ -11,8 +12,9 @@ const UserProfilePopUp = ({chatPopUp, setChatPopUp}) => {
           <div className='popup-inside'>
           <img className='avatar-popup' src={avatar}></img>
           <button className='closeprofile-btn' onClick={() => setChatPopUp(!chatPopUp)}>X</button>
-          <h2 className='username-popup'>Mariainny</h2>
-              <h3 className='location-popup'>Manhattan</h3>
+          <h2 className='username-popup'>Dorito</h2>
+              <h3 className='location-popup'>Jersey City</h3>
+              <img className='chat-img' src={chat}></img>
           </div>
       </div>
     ) : ""
